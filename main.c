acd60e52e5e7f5aa75d4b7d2f5c531a352b783b0
feitosadavi <davifeitosa.dev@protonmail.com>
2024-06-18 02:34:33 -0300

    .

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <locale.h>
 #include "menus/menus.h"
-
 int main() {
+  setlocale(LC_ALL, "Portuguese");
   abrir_menu_principal("");
   return 0;
 }
