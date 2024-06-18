ddeedc1dcff6db6635a07d36cce405a8f1157671
feitosadavi <davifeitosa.dev@protonmail.com>
2024-06-18 01:05:16 -0300

    .

--- conta/encerrar_conta.c
+++ conta/encerrar_conta.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../menus/menus.h"
+#include "../gerenciador_de_arquivos.h"
 
 void encerrar_conta()
 {
+  char *n_conta;
+
   system("clear");
   printf("Encerrar conta: não implementado");
+  printf("Digite o número da conta para encerrar: ");
+  scanf("%s", n_conta);
   abrir_menu_funcionario();
 }
